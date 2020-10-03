2c6301a4bab157550332525cae4b482bb623761e
david-256 <62044846+david-256@users.noreply.github.com>
2020-10-03 12:54:08 -0500

    Add files via upload

---
+++ CreateTwoVMsGit.ps1
@@ -0,0 +1,30 @@
+﻿#Two machines full creation
+
+#Resource group name
+$ResourceGroup = D-rg
+$Vnet = D-net
+$Location = "EastUS" 
+
+New-AzResourceGroup -Name $ResourceGroup  -Location $Location
+
+#Subnet and Vnet
+$Subnet= New-AzVirtualNetworkSubnetConfig -Name default -AddressPrefix 10.0.0.0/24
+New-AzVirtualNetwork -Name $Vnet -ResourceGroupName $ResourceGroup -Location $Location -AddressPrefix 10.0.0.0/16 -Subnet $Subnet
+
+ # Two Vnet
+ #Create 2 VMs
+$vm1 = New-AzVm `
+ -ResourceGroupName $ResourceGroup `
+ -Name vm1 `
+ -VirtualNetworkName $Vnet `
+ -SubnetName default `
+ -image Win2016Datacenter `
+ -Size Standard_B1S
+
+ $vm2 = New-AzVm `
+ -ResourceGroupName $ResourceGroup `
+ -Name "vm2" `
+ -VirtualNetworkName $Vnet `
+ -SubnetName "default" `
+ -image "Win2016Datacenter" `
+ -Size "Standard_B1S"
